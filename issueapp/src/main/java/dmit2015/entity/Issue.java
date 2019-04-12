3d30584bbdaff62e29f2523adc1d00231911503c
Sam Wu <swu@nait.ca>
2019-04-12 14:14:26 -0600

    Completed version of issue CRUD app

--- issueapp/src/main/java/dmit2015/entity/Issue.java
+++ issueapp/src/main/java/dmit2015/entity/Issue.java
@@ -10,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -22,9 +24,11 @@ public class Issue implements Serializable {
 	@GeneratedValue
 	private long issueId;
 	
+	@NotBlank(message="Enter an issue to report")
 	@Column(nullable=false, length=99)
 	private String issueLabel;
 	
+	@NotNull(message="Enter or select the Date of the issue")
 	@Column(nullable=false)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date issueDate = Calendar.getInstance().getTime();
